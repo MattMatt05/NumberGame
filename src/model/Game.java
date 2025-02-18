4f7a8ca60baa606099c8b6de6da5e9fdee757b26
Matthew M <molskimatthew@gmail.com>
2025-02-17 16:54:38 -0800

    Implemented Game Class

---
+++ src/model/Game.java
@@ -0,0 +1,48 @@
+package model;
+
+// Game is responsible for both the actions and setup for the game.
+public class Game {
+    private int total;
+    private int turn;
+    private int playerMove;
+    private String playerName;
+
+
+    public Game() {
+        total = (int) ((Math.random() * 11) + 20);
+        turn = (int) (Math.random() * 3);
+    }
+
+    // EFFECTS: sets playerMove
+    public void setPlayerMove(int playerMove) {
+        this.playerMove = playerMove;
+    }
+
+    // EFFECTS: returns playerMove
+    public int getPlayerMove() {
+        return playerMove;
+    }
+
+    // EFFECTS: computer chooses its next move
+    public int computerTurn() {
+        if ((total % 4) == 1) { // ex: 5
+            return 1;
+        } else if ((total % 4) == 2) { // ex: 6
+            return 2;
+        } else if ((total % 4) == 3) { // ex: 7
+            return 3;
+        } else {
+            return (int) (Math.random() * 4);
+        }
+    }
+
+    // EFFECTS: returns the name of the current player
+    public String getCurrentTurn() {
+        if (turn == 1) {
+            return playerName;
+        } else {
+            return "CPU";
+        }
+    }
+    
+}
